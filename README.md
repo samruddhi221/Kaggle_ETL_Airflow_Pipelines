cfbd2b7b52606be7f4d316ffd75ad08eb17a4313
samruddhi221 <samruddhinarendra.patil@sjsu.edu>
2021-09-06 20:23:21 -0700

    added dataset download stage using kaggle APIs

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # KAGGLE ETL using AIRFLOW
 
+## Installations
+- Create API token for kaggle by following https://github.com/Kaggle/kaggle-api#api-credentials and place `kaggle.json`
+in `${HOME}/.kaggle/kaggle.json`
+
+## Caveats 
+- If you encounter 403 forbidden error on downloading kaggle files, make sure you have accepted the competition rules. 
--- dags/nlp-etl-dag.py
+++ dags/nlp-etl-dag.py
@@ -3,6 +3,7 @@ from airflow import DAG
 from datetime import timedelta
 from airflow.utils.dates import days_ago
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
 
 default_args = {
     'owner': 'airflow',
@@ -14,6 +15,10 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
+default_config = {
+    'competition_name': 'jigsaw-multilingual-toxic-comment-classification'
+}
+
 dag = DAG('nlp-etl',
           default_args=default_args,
           description='Kaggle NLP ETL pipeline',
@@ -21,16 +26,18 @@ dag = DAG('nlp-etl',
           start_date=days_ago(2),
           tags=['kaggle', 'nlp', 'etl'])
 
-
-def t1_func():
-    print("this is task-1.")
+dag.params = default_config
 
 
-def t2_func():
+def t2_func(**context):
     print("this is task-2.")
 
 
 with dag:
-    t1 = PythonOperator(task_id='task1', python_callable=t1_func)
+    t1 = BashOperator(
+        task_id='download_dataset',
+        bash_command=
+        'kaggle competitions download {{ dag_run.conf["competition_name"]}} -p /tmp'
+    )
     t2 = PythonOperator(task_id='task2', python_callable=t2_func)
     t1 >> t2
