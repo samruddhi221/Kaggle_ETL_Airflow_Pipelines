94bd93c3f4fc7e2e3376933b3b67c73af7ad5dee
samruddhi221 <samruddhinarendra.patil@sjsu.edu>
2021-09-07 20:34:36 -0700

    parsed raw dataset to pandas df and saved as hdf5

--- dags/nlp-etl-dag.py
+++ dags/nlp-etl-dag.py
@@ -9,6 +9,7 @@ from kaggle_etl import dataloader_factory
 from kaggle_etl.dataloader_factory import DataloaderFactory
 
 import os
+import pandas as pd
 
 default_args = {
     'owner': 'airflow',
@@ -37,28 +38,30 @@ dag.params = default_config
 def dataloader_helper(competition_name):
     dataloader_fn = DataloaderFactory(competition_name).get_loader()
     out_dicts = dataloader_fn(os.path.join('/tmp', competition_name))
-    out_dicts['train'].to_pickle(
-        os.path.join('/tmp', competition_name, 'train.pkl'))
-    out_dicts['validation'].to_pickle(
-        os.path.join('/tmp', competition_name, 'validation.pkl'))
-    out_dicts['test'].to_pickle(
-        os.path.join('/tmp', competition_name, 'test.pkl'))
+    out_dicts['train'].to_hdf(
+        os.path.join('/tmp', competition_name, 'train.h5'), key='df', mode='w')
+    out_dicts['validation'].to_hdf(
+        os.path.join('/tmp', competition_name, 'validation.h5'), key='df', mode='w')
+    out_dicts['test'].to_hdf(
+        os.path.join('/tmp', competition_name, 'test.h5'), key='df', mode='w')
+    print("----------------------------------------------")
+    test_df = pd.read_hdf(os.path.join('/tmp', competition_name, 'test.h5'), 'df') 
+    print(test_df.head())
 
 
 with dag:
-    # download_dataset = BashOperator(
-    #     task_id='download_dataset',
-    #     bash_command=
-    #     'kaggle competitions download {{ dag_run.conf["competition_name"]}} -p /tmp'
-    # )
-    dummy_op1 = DummyOperator(task_id='dummy_operator')
-    # extract_dataset = BashOperator(
-    #     task_id='extract_dataset',
-    #     bash_command=
-    #     'mkdir -p /tmp/{{ dag_run.conf["competition_name"]}} && unzip /tmp/{{dag_run.conf["competition_name"]}}.zip -d /tmp/{{ dag_run.conf["competition_name"]}}'
-    # )
+    download_dataset = BashOperator(
+        task_id='download_dataset',
+        bash_command=
+        'kaggle competitions download {{ dag_run.conf["competition_name"]}} -p /tmp'
+    )
+    extract_dataset = BashOperator(
+        task_id='extract_dataset',
+        bash_command=
+        'mkdir -p /tmp/{{ dag_run.conf["competition_name"]}} && unzip /tmp/{{dag_run.conf["competition_name"]}}.zip -d /tmp/{{ dag_run.conf["competition_name"]}}'
+    )
     create_df = PythonOperator(
         task_id='create_dataframes',
         python_callable=dataloader_helper,
         op_args=['{{dag_run.conf["competition_name"]}}'])
-    dummy_op1 >> create_df
+    download_dataset >> extract_dataset >> create_df
